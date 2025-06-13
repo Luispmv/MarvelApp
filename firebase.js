8fa931651086d50454df06e860281c1d1492c636
lpmvdev7 <lpmvdev7@gmail.com>
2025-06-13 02:11:54 -0600

    Intento de firebase #1

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import * as firebase from "firebase"
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyClppC94w2jLd6AnzBmx8jtkLiW_edlrIo",
+  authDomain: "fir-react-3ec54.firebaseapp.com",
+  projectId: "fir-react-3ec54",
+  // storageBucket: "fir-react-3ec54.firebasestorage.app",
+  storageBucket: "fir-react-3ec54.appspot.com",
+  messagingSenderId: "1011837157609",
+  appId: "1:1011837157609:web:af9949a15641ad9d76b078"
+};
+
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+const auth = getAuth(app);
+export { auth };
