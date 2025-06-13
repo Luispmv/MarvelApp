4a3b668a1cf8227d3b38516b1e92f0db0150a631
lpmvdev7 <lpmvdev7@gmail.com>
2025-06-13 02:53:31 -0600

    Registro de usuarios listo

--- src/screens/RegistroScreen.js
+++ src/screens/RegistroScreen.js
@@ -1,5 +1,9 @@
 import { useNavigation } from "@react-navigation/native";
+import { useEffect, useState } from "react";
 import { ScrollView, Text, View, StyleSheet, Image, TextInput, TouchableOpacity } from "react-native";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebase';
+
 
 export default function RegistroScreen(){
     return(
@@ -35,18 +39,42 @@ const estilosImagen = StyleSheet.create({
 
 function FormularioRegistro(){
 
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     const navegacion = useNavigation()
+
+    useEffect(()=>{
+        const unsubscribe = auth.onAuthStateChanged(user =>{
+            if(user){
+                navegacion.replace("HomeScreen")
+            }
+        })
+
+        return unsubscribe
+    }, []) 
+
+    const handleSignUp = () => {
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user.email);
+        })
+        .catch((error) => {
+            alert(error.message);
+        });
+    };
 
     return(
         <View style={form.container}>
             <View style={form.contenedorInputs}>
                 <Text style={form.titulo}>Registro</Text>
-                <TextInput style={form.inputEmail} placeholder="Coloca tu email" placeholderTextColor={"#929090"}></TextInput>
-                <TextInput style={form.inputContraseña} placeholder="Coloca tu contraseña" placeholderTextColor={"#929090"} secureTextEntry></TextInput>
+                <TextInput style={form.inputEmail} placeholder="Coloca tu email" placeholderTextColor={"#929090"} value={email} onChangeText={text=> setEmail(text)}></TextInput>
+                <TextInput style={form.inputContraseña} placeholder="Coloca tu contraseña" placeholderTextColor={"#929090"} secureTextEntry value={password} onChangeText={text=>setPassword(text)}></TextInput>
             </View>
 
             <View style={form.contendorBotones}> 
-                <TouchableOpacity style={form.botonInicioSesion}>
+                <TouchableOpacity style={form.botonInicioSesion} onPress={handleSignUp}>
                     <Text style={form.botonInicioSesionTexto}>Registro</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={form.botonRegistro}
