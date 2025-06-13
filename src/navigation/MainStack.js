b307a6b676f69721739c0bb43d6672566188363f
lpmvdev7 <lpmvdev7@gmail.com>
2025-06-13 01:59:05 -0600

    Agregando un nuevo stack: RegistroScreen.js

--- src/navigation/MainStack.js
+++ src/navigation/MainStack.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import LoginScreen from "../screens/LoginScreen";
+import RegistroScreen from "../screens/RegistroScreen";
 
 const { createNativeStackNavigator } = require("@react-navigation/native-stack");
 
@@ -12,8 +13,10 @@ export default function MainStack(){
                 screenOptions={{headerShown:false}}
             >
                 <Stack.Screen name="LoginScreen" component={LoginScreen}>
-
                 </Stack.Screen>
+
+                <Stack.Screen name="RegistroScreen" component={RegistroScreen}></Stack.Screen>
+
             </Stack.Navigator>
         </NavigationContainer>
     )
